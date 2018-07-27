3ee69290623ce3b2d3dacd114c794cfcd28b5a99
Christopher Goes <goesc@acm.org>
2018-07-26 18:32:31 -0600

    Install ssh config for Linux and WSL

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Loads configurations into their proper places
 
-if [ ! -d "./configs" ]; then
+if [ ! -d "./configs" ] ; then
     echo "Could not find configs. This script must be run in the root sneaky-scripts directory."
     exit 1
 fi
@@ -11,3 +11,9 @@ cat ./configs/bash_profile >> ~/.bash_profile
 cat ./configs/profile >> ~/.profile
 cat ./configs/bashrc >> ~/.bashrc
 cat ./configs/gitconfig >> ~/.gitconfig
+cat ./configs/ssh_config >> ~/.ssh/config
+
+if [ $WSL ] ; then
+    echo "Installing SSH key for WSL..."
+    cat ./configs/ssh_config >> "/mnt/c/Users/{$WINUSER}/.ssh/config"
+fi
